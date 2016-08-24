5f7afa63e21a713fb3e43d999d521d6d469da618
subhadeepmukherjee <Subhadeep.Mukherjee@outlook.com>
2016-08-24 20:37:14 +0530

    Subhadeep Mukherjee: Initial commit for developemnt

---
+++ longrunningtask.js
@@ -0,0 +1,16 @@
+﻿onmessage = function (event) {
+    var sum = 0;
+    var currentPercentageComplete = 0;
+    var maxLimit = event.data;
+        for(var j= 0; j<= maxLimit; j++){
+            sum += j;
+            newPercentageComplete = Math.round((j / maxLimit) * 100);
+            if (newPercentageComplete > currentPercentageComplete) {
+                //send message back to main page thread
+                postMessage(newPercentageComplete + '% complete');
+                //update current percentage complete
+                currentPercentageComplete = newPercentageComplete;
+            }
+        }
+        postMessage('Sum = ' + sum);
+}
